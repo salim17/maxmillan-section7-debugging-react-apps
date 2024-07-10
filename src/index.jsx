3c7934d91b8702662c29cd262b2c150973eafc1c
Mohd Salim <saalim9965@gmail.com>
2024-07-10 18:43:09 +0530

    section 7: lecture 128 understanding react strict mode.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App.jsx';
-import './index.css';
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// can wrap any component to strict mode. not necessary to wrap app component. can wrap only parts of application components inside
+// strict mode executes the component twice helping us find the problem. It does only in development mode.
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
